433b16ec8379f12ebf6d7867b6b7fa21c843f743
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2017-12-19 01:18:41 +0300

    cache fix

--- ajax/templates/edu.php
+++ ajax/templates/edu.php
@@ -100,5 +100,3 @@
         <div id="curve_chart_files<?php echo $arResult["repository_id"] . "_" . $period?>" style="width: 370px; height: 200px; display: inline-block"></div>
         <div id="curve_chart_lines<?php echo $arResult["repository_id"] . "_" . $period?>" style="width: 370px; height: 200px; display: inline-block;"></div>
     </div>
-
-<hr>
