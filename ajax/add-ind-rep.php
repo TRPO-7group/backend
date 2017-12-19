17618697840f7d2b2540a8c8fd2516a71bdaf883
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2017-12-19 12:08:52 +0300

    hot-fix

--- ajax/add-ind-rep.php
+++ ajax/add-ind-rep.php
@@ -1,12 +1,11 @@
 <?php
 require $_SERVER["DOCUMENT_ROOT"]. "/reposit-catalog/local/lib/php_init.php";
-
 $user = MainClass::getUser();
 
 if ($user["user_id"]) {
     $url = $_POST["rep_url"];
     $descr = $_POST["rep_descr"];
-    $list = DB::getList("rep", "rep_id", false, "rep_url=" . $url);
+    $list = DB::getList("rep", "rep_id", false, "rep_url='" . $url . "'");
     if (!count($list))
         DB::insertRow("rep", array("rep_url", "rep_description", "rep_owner", "is_ind"), array($url, $descr, $user["user_id"], MainClass::$INDIVIDUAL));
 }
